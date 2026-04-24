a97c86fdb5b4e2b653371ec6222d8f305f1fd58b
Oliver <145952622+GummyHat@users.noreply.github.com>
2026-04-24 14:14:00 -0400

    This is the template for the cuda file
    This does not work, yet!

---
+++ Sobel.cu
@@ -0,0 +1,21 @@
+#include <cstddef>
+#include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include "pixel.h"
+
+extern "C"
+{
+
+}
+
+// CUDA kernal to perform Sobel calulation on each pixel
+void sobel()
+{
+
+}
+
+void runSobelOnPixels( const size_t sizeOfArray, pixel *prev_array_upper, pixel *next_array_lower, const size_t WIDTH, const size_t LENGTH, const int rank)
+{
+
+}
