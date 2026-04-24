29873c0ffb99a119401f7760bbd77c88b645391a
Oliver <145952622+GummyHat@users.noreply.github.com>
2026-04-24 13:56:18 -0400

    Update Sobel.c
    added send recv functionality

--- Sobel.c
+++ Sobel.c
@@ -52,16 +52,17 @@ int main(int argc, char* argv[])
     const size_t pixelsPerProc = (LENGTH * WIDTH) / nprocs;
 
     //This is remainder pixels for the last process
+    //This is currently unused, but should be used! Ranks currently need to be divisible by pixel count.
     const size_t remainderPixelsPerProc = (LENGTH * WIDTH) % nprocs;
 
     //This is an array for each process that contains all its pixels
-    pixel* pixelsInProc = malloc(sizeof(pixel) * pixelsPerProc);
+    pixel* myPixels = malloc(sizeof(pixel) * pixelsPerProc);
 
     unsigned char* pixelBuffer = malloc(sizeof(unsigned char) * 3);
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    //READ FILES INTO EACH PROCESS
+    //READ FILE INTO EACH PROCESS
     char *filename = argv[4];
     MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
     MPI_Offset offset = 0;
@@ -73,18 +74,61 @@ int main(int argc, char* argv[])
     {
         offset = (i * pixelsPerProc) * nprocs; 
         MPI_File_read_at(fh, offset, pixelBuffer, 3, MPI_CHAR, &status);
-        pixelsInProc->red = pixelBuffer[0];
-        pixelsInProc->green = pixelBuffer[1];
-        pixelsInProc->blue = pixelBuffer[2];
+        myPixels->red = pixelBuffer[0];
+        myPixels->green = pixelBuffer[1];
+        myPixels->blue = pixelBuffer[2];
     }
     MPI_File_close(&fh);
     free(pixelBuffer);
     MPI_Barrier(MPI_COMM_WORLD);
 
 
-    //CUDA CALLS HERE
+    //Exchange next proc and prev procs pixel arrays
+    MPI_Request requests[4];
+    int request_count = 0;
+    
+    pixel *prev_array_upper = malloc(sizeof(pixel) * pixelsPerProc);
+    pixel *next_array_lower = malloc(sizeof(pixel) * pixelsPerProc);
+    if(rank == 0)
+    {
+        for(int i = 0; i < pixelsPerProc; ++i)
+        {
+            //set it to black for now
+            prev_array_upper->red = 0;
+            prev_array_upper->green = 0;
+            prev_array_upper->blue = 0;
+        }
+        MPI_Irecv(next_array_lower, pixelsPerProc, MPI_INT, rank + 1, 100, MPI_COMM_WORLD, &requests[request_count++]);
+        MPI_Isend(myPixels, pixelsPerProc, MPI_INT, rank + 1, 200, MPI_COMM_WORLD, &requests[request_count++]);
+    }
+    else if(rank == nprocs - 1)
+    {
+        for(int i = 0; i < pixelsPerProc; ++i)
+        {
+            //set it to black for now
+            next_array_lower->red = 0;
+            next_array_lower->green = 0;
+            next_array_lower->blue = 0;
+        }
+        MPI_Irecv(prev_array_upper, pixelsPerProc, MPI_INT, rank - 1, 200, MPI_COMM_WORLD, &requests[request_count++]);
+        MPI_Isend(myPixels, pixelsPerProc, MPI_INT, rank - 1, 100, MPI_COMM_WORLD, &requests[request_count++]);
+    }
+    else {
+        MPI_Irecv(prev_array_upper, pixelsPerProc, MPI_INT, rank - 1, 200, MPI_COMM_WORLD, &requests[request_count++]);
+        MPI_Irecv(next_array_lower, pixelsPerProc, MPI_INT, rank + 1, 100, MPI_COMM_WORLD, &requests[request_count++]);
+        MPI_Isend(myPixels, pixelsPerProc, MPI_INT, rank + 1, 200, MPI_COMM_WORLD, &requests[request_count++]);
+        MPI_Isend(myPixels, pixelsPerProc, MPI_INT, rank - 1, 100, MPI_COMM_WORLD, &requests[request_count++]);
+    }
 
-    free(pixelsInProc);
+    MPI_Waitall(request_count, requests, MPI_STATUSES_IGNORE);
+
+    //CUDA CALL HERE
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    free(myPixels);
+    free(prev_array_upper);
+    free(next_array_lower);
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
     return 0;
