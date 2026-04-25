453debd3eb3911b9c80c66b45b7337997563309c
Oliver <145952622+GummyHat@users.noreply.github.com>
2026-04-25 15:04:18 -0400

    Update pixel.h

--- source/pixel.h
+++ source/pixel.h
@@ -1,10 +1,23 @@
 #ifndef PIXEL_H
 #define PIXEL_H
 
+//Each pixel is 3 bytes
 typedef struct {
     unsigned char red;
     unsigned char green;
     unsigned char blue;
 } pixel;
+
+typedef struct {
+    unsigned char current;
+    unsigned char topLeft;
+    unsigned char topMiddle;
+    unsigned char topRight;
+    unsigned char left;
+    unsigned char right;
+    unsigned char bottomLeft;
+    unsigned char bottomMiddle;
+    unsigned char bottomRight;
+} surroundingPixels;
 
 #endif
